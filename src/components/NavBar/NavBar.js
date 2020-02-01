2010bfb4426528545a97c168ffbf8d70087c5fec
Elco Garcia <garciaelco18@gmail.com>
2020-01-31 18:59:21 -0600

    fixing navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import style from './NavBar.module.css';
-import { Link, Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 const NavBar = () => {
 	return (
 		<div className={style.navBar}>
-			<Router>
-				<Link to="/about" className={style.navItem}>
-					About
-				</Link>
-				<Link to="/resume" className={style.navItem}>
-					Resume
-				</Link>
-				<Link to="/resume" className={style.navItem}>
-					Contact
-				</Link>
-			</Router>
+			{/* <Switch>
+				<Route path="/about" className={style.navItem}>
+					About />
+				</Route>
+				<Route to="/resume" className={style.navItem}>
+					Resume />
+				</Route>
+				<Route path="/contact" className={style.navItem}>
+					Contact />
+				</Route>
+			</Switch> */}
 		</div>
 	);
 };
