44668d962fae09461f3843f983948fbdcf7771f6
Elco Garcia <garciaelco18@gmail.com>
2020-02-06 12:14:18 -0600

    restarting from scratch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Title from '../src/components/Title/Title';
-import NavBar from '../src/components/NavBar/NavBar';
-import Portfolio from '../src/components/Portfolio/Portfolio';
 
 const App = () => {
 	return (
 		<div>
-			<NavBar />
-			<Title />
-			<Portfolio />
+			<h1>Placeholder</h1>
 		</div>
 	);
 };
